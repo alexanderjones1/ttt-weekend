ae81f32444a9105fab7bb63115c2e4b402a9f16d
alex-jones <abjhoops@gmail.com>
2022-12-09 14:08:53 -0700

    complete step 3 and begin 4

--- js/app.js
+++ js/app.js
@@ -4,15 +4,55 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board, element, winner, tie
 
 /*------------------------ Cached Element References ------------------------*/
 
+// // Step 2 - Store cached element references.
 
+const squareEls = document.querySelectorAll('.sqr')
+
+const messageEl = document.getElementById('message')
+console.log(message);
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
-
 /*-------------------------------- Functions --------------------------------*/
 
+
+init()
+function init() {
+    board = [null, null, null, null, null, null, null, null, null, ]
+    element = -1
+    winner = false
+    tie = false
+    render()
+}
+
+function render() {
+    updateBoard()
+    updateMessage()
+}
+
+function updateBoard() {
+    board.forEach((element, idx) => {
+        if (element === 1) {
+            squareEls[idx].innerText = 'x'
+        } else if (element === -1) {
+            squareEls[idx].innerText = 'o'
+        } else {
+            squareEls[idx].innerText = ''
+        }
+    })
+}
+
+// function updateMessage() {
+//     if (winner === false && tie === false) {
+//         messageEl.textContent =
+//     } else if (winner === false && tie === true) {
+//         messageEl.textContent = "You Tied!"
+//     } else {
+//         messageEl.textContent = " Congratulations! You Win!"
+//     }
+// }
