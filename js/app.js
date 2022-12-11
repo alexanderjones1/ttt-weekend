0a05acd50d8b4c05cce335c60956f3fb45d5ace6
alex-jones <abjhoops@gmail.com>
2022-12-11 15:24:58 -0700

    complete step 5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -50,11 +59,12 @@ function updateBoard() {
 function updateMessage() {
     if (winner === false && tie === false) {
         messageEl.textContent = (turn === 1 ? 'Player X turn.' : 'Player O turn')
-        console.log(messageEl);
     } else if (winner === false && tie === true) {
         messageEl.textContent = "You Tied!"
     } else {
         messageEl.textContent = " Congratulations! You Win!"
     }
 }
+
+
 
